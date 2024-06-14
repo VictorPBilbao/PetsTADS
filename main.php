914c23b745fa592b988ff80ac64c32180c6ebd8e
VictorPBilbao <victor.pasini.bilbao@gmail.com>
2024-06-13 21:44:50 -0300

    chore: Update main.php to create a new table and insert data for dogs

--- main.php
+++ main.php
@@ -16,48 +16,48 @@ $token = $db->signin([
     "pass" => "root"
 ]);
 
-// // create a new table
-// $db->query("DEFINE TABLE IF NOT EXISTS dog SCHEMAFULL");
+// create a new table
+$db->query("DEFINE TABLE IF NOT EXISTS dog SCHEMAFULL");
 
-// // insert schema
-// $db->query("
-//     DEFINE FIELD name ON TABLE dog TYPE string;
-//     DEFINE FIELD image ON TABLE dog TYPE string;
-//     DEFINE FIELD description ON TABLE dog TYPE string;
-// ");
+// insert schema
+$db->query("
+    DEFINE FIELD name ON TABLE dog TYPE string;
+    DEFINE FIELD image ON TABLE dog TYPE string;
+    DEFINE FIELD description ON TABLE dog TYPE string;
+");
 
-// // insert data
+// insert data
 
-// $db->query("
-//     CREATE dog CONTENT {
-//         name: 'Dog 1',
-//         image: 'https://www.google.com',
-//         description: 'This is a dog'
-//     }
-// ");
+$db->query("
+    CREATE dog CONTENT {
+        name: 'Dog 1',
+        image: 'https://www.google.com',
+        description: 'This is a dog'
+    }
+");
 
-// $db->query("
-//     CREATE dog CONTENT {
-//         name: 'Dog 2',
-//         image: 'https://www.google.com',
-//         description: 'This is yet another dog'
-//     }
-// ");
+$db->query("
+    CREATE dog CONTENT {
+        name: 'Dog 2',
+        image: 'https://www.google.com',
+        description: 'This is yet another dog'
+    }
+");
 
-// $db->query("
-//     CREATE dog CONTENT {
-//         name: 'Dog 3',
-//         image: 'https://www.google.com',
-//         description: 'This is also another dog'
-//     }
-// ");
+$db->query("
+    CREATE dog CONTENT {
+        name: 'Dog 3',
+        image: 'https://www.google.com',
+        description: 'This is also another dog'
+    }
+");
 
-// // Create a new person in the database with a custom id.
-// $person = $db->create("dog", [
-//     "name" => "Dog 4",
-//     "image" => "https://www.google.com",
-//     "description" => "This is also another dogggg"
-// ]);
+// Create a new person in the database with a custom id.
+$person = $db->create("dog", [
+    "name" => "Dog 4",
+    "image" => "https://www.google.com",
+    "description" => "This is also another dogggg"
+]);
 
 $total_dogs = $db->query("SELECT name FROM dog")[0]['result'];
 
