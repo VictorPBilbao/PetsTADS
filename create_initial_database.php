e0a3c12a92633cc37d359e356bc212f9367357f1
VictorPBilbao <victor.pasini.bilbao@gmail.com>
2024-06-15 22:11:44 -0300

    Update image URLs for Pets in create_initial_database.php

--- create_initial_database.php
+++ create_initial_database.php
@@ -12,7 +12,7 @@ DEFINE FIELD description ON TABLE Pets TYPE string;
 $result = runDbCommand("
 CREATE Pets CONTENT {
     name: 'Belinha',
-    image: 'uploads/belinha.jpg',
+    image: 'https://storage.googleapis.com/pets_uploads/belinha.jpg',
     description: 'É uma cachorra muito brincalhona e adora correr no parque. Seu hobby favorito é morder os chinelos do dono e de vez em quando, roubar um pedaço de carne da mesa. Ela é muito carinhosa e adora um carinho na barriga.'
 }
 ");
@@ -20,7 +20,7 @@ CREATE Pets CONTENT {
 $result = runDbCommand("
 CREATE Pets CONTENT {
     name: 'Frajola',
-    image: 'uploads/frajola.jpeg',
+    image: 'https://storage.googleapis.com/pets_uploads/frajola.jpeg',
     description: 'É um gato muito preguiçoso e adora dormir o dia todo. Seu hobby favorito é deitar no sol e dormir. Ele é muito carinhoso e adora um carinho na barriga. Além disso, ele é muito brincalhão e adora brincar com bolinhas de lã.'
 }
 ");
@@ -28,7 +28,15 @@ CREATE Pets CONTENT {
 $result = runDbCommand("
 CREATE Pets CONTENT {
     name: 'Nemo',
-    image: 'uploads/nemo.jpg',
+    image: 'https://storage.googleapis.com/pets_uploads/nemo.jpg',
     description: 'É um peixe muito bonito e adora nadar no aquário. Seu hobby favorito é nadar e brincar com as plantas do aquário. Ele é muito carinhoso e adora um carinho na barriga. Além disso, ele é muito brincalhão e adora brincar com os outros peixes.'
+}
+");
+
+$result = runDbCommand("
+CREATE Pets CONTENT {
+    name: 'Luna',
+    image: 'https://storage.cloud.google.com/pets_uploads/luna.jpeg',
+    description: 'A Luna é uma Weinmaraner muito brincalhona e adora correr atrás de capivaras no parque. Ela não gosta de receber abraços e beijos mas adora um carinho na barriga nos dias quentes e de brincar com bolinhas de tênis.'
 }
 ");
